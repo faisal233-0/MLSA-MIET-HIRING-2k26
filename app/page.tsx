91130a306c9398c371d01f6716f9c037b9212965
faisal233-0 <faisal233-0@users.noreply.github.com>
2026-08-15 14:02:19 +0530

    date tesing

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ const RegistrationCountdown = () => {
 // .......... HIRING TIMELINE COMPONENT  ............
 const HiringTimeline = () => {
     const timelineEvents = [
-        { date: 'Aug 17, 2026', title: 'Applications Open', description: 'The hiring portal goes live. Submit your applications to join the team.' },
+        { date: 'Aug 14, 2026', title: 'Applications Open', description: 'The hiring portal goes live. Submit your applications to join the team.' },
         { date: 'Aug 31, 2026', title: 'Applications Close', description: 'Last day to submit your application. Don\'t miss out!' },
         { date: 'Sep 7th and 8th, 2026', title: 'Interview Rounds', description: 'Shortlisted candidates will be contacted for interviews.' },
         { date: 'Sep 11, 2026', title: 'Results Announced', description: 'Welcome to the new members of the MLSA MIET Chapter!' },
