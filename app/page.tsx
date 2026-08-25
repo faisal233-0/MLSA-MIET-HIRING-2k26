d49f77a3ca3d0a44db92d73a0f2f428142a3f2f1
faisal233-0 <faisal233-0@users.noreply.github.com>
2026-08-25 22:29:38 +0530

    timeline message update

--- app/page.tsx
+++ app/page.tsx
@@ -149,7 +149,7 @@ const HiringTimeline = () => {
     const timelineEvents = [
         { date: 'Aug 17, 2026', title: 'Applications Open', description: 'The hiring portal goes live. Submit your applications to join the team.' },
         { date: 'Aug 31, 2026', title: 'Applications Close', description: 'Last day to submit your application. Don\'t miss out!' },
-        { date: 'Sep 9th and 10th, 2026', title: ' Rounds', description: 'Shortlisted candidates will be contacted for interviews.' },
+        { date: 'Sep 9th and 10th, 2026', title: ' Rounds', description: 'Shortlisted candidates will be contacted for the interviews.' },
         { date: 'Sep 12, 2026', title: 'Results Announced', description: 'Welcome to the new members of the MLSA MIET Chapter!' },
     ];
 
